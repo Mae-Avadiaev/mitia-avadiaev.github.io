c4d49a9a1616c7ce0c9521f03b944dff068c32e3
Mitya Avadyaev <mitya.avadyaev@gmail.com>
2022-05-04 00:01:42 +0300

    removed icon dep

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,5 @@
 import themeIcon from "../images/theme-icon.png"
-import menuIcon from "../images/menu-icon.png"
-import educationIcon from "../images/education.png"
 import terminalIcon from "../images/Terminal-icon-Yosemite.png"
-// import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
     return (
